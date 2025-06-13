1ec7c9e81cb15ea1f46894524f6736dfbc319039
AnilBASARAN <nlbsrn@gmail.com>
2025-06-13 03:50:18 +0300

    created Die Class and getter setter functionality

---
+++ main.py
@@ -0,0 +1,11 @@
+class Die:
+    def __init__(self):
+        self._value = None
+
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self,new_color):
+        self._value = new_color
