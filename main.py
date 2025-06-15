5a271f4772127d6022371ff911ce534bae411ab8
AnilBASARAN <nlbsrn@gmail.com>
2025-06-15 05:23:08 +0300

    last touches

--- main.py
+++ main.py
@@ -19,13 +19,56 @@ class Player:
     def __init__(self,die,is_computer=False):
         self._die = die
         self._is_computer = is_computer
+        self._counter = 10
+
+    @property
+    def die(self):
+        return self._die
+    
+    @property
+    def is_computer(self):
+        return self._is_computer
+    
+    @property
+    def counter(self):
+        return self._counter
+    
+    def increment_counter(self):
+        self._counter += 1
+
+    def decrement_counter(self):
+        self._counter -= 1
 
     def roll_dice(self):
         return self._die.roll()
+    
+class Game_round:
+    def __init__(self):
+    
+        
+        print("*********This is the new Round*********")
+        input("*****Press Any Key to Roll the Dice*****")
 
 
-my_die = Die()
-my_player = Player(my_die)
 
-my_player.roll_dice()
-print(my_die.value)
+
+
+class DiceGame:
+    def __init__(self,player,computer):
+         self._player = player
+         self._computer = computer
+         self._round = round
+
+    def play(self):
+        print("*****************************")
+        print("***Welcome to My Dice Game***")
+        print("*****************************")
+        while True:
+            self.play_round()
+
+    def play_round(self):
+        print("------New Round------")
+        input("----Press Any Key----")
+
+        player_value = self._player.roll_dice()
+        computer_value = self._computer.roll_dice()
